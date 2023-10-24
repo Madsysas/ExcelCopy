ff16a668655c1f7443e71f95a22bfcc5584c4e00
Sysas <liudas_rolandas@yahoo.com>
2023-10-24 18:28:53 +0300

    Last

---
+++ ExcelCopy/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ExcelCopy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nuo_sav_textbox = new System.Windows.Forms.TextBox();
+            this.Iki_sav_textbox = new System.Windows.Forms.TextBox();
+            this.Nuo_sav_label = new System.Windows.Forms.Label();
+            this.Iki_sav_label = new System.Windows.Forms.Label();
+            this.Pasirink_file_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Copy_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Nuo_sav_textbox
+            // 
+            this.Nuo_sav_textbox.Location = new System.Drawing.Point(168, 19);
+            this.Nuo_sav_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Nuo_sav_textbox.Name = "Nuo_sav_textbox";
+            this.Nuo_sav_textbox.Size = new System.Drawing.Size(161, 26);
+            this.Nuo_sav_textbox.TabIndex = 1;
+            this.Nuo_sav_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Nuo_sav_textbox_KeyPress);
+            // 
+            // Iki_sav_textbox
+            // 
+            this.Iki_sav_textbox.Location = new System.Drawing.Point(168, 55);
+            this.Iki_sav_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Iki_sav_textbox.Name = "Iki_sav_textbox";
+            this.Iki_sav_textbox.Size = new System.Drawing.Size(161, 26);
+            this.Iki_sav_textbox.TabIndex = 2;
+            this.Iki_sav_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Iki_sav_textbox_KeyPress);
+            // 
+            // Nuo_sav_label
+            // 
+            this.Nuo_sav_label.AutoSize = true;
+            this.Nuo_sav_label.Location = new System.Drawing.Point(36, 22);
+            this.Nuo_sav_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Nuo_sav_label.Name = "Nuo_sav_label";
+            this.Nuo_sav_label.Size = new System.Drawing.Size(124, 20);
+            this.Nuo_sav_label.TabIndex = 0;
+            this.Nuo_sav_label.Text = "Pradzios savaite";
+            // 
+            // Iki_sav_label
+            // 
+            this.Iki_sav_label.AutoSize = true;
+            this.Iki_sav_label.Location = new System.Drawing.Point(36, 58);
+            this.Iki_sav_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Iki_sav_label.Name = "Iki_sav_label";
+            this.Iki_sav_label.Size = new System.Drawing.Size(129, 20);
+            this.Iki_sav_label.TabIndex = 0;
+            this.Iki_sav_label.Text = "Pabaigos savaite";
+            // 
+            // Pasirink_file_button
+            // 
+            this.Pasirink_file_button.Location = new System.Drawing.Point(166, 91);
+            this.Pasirink_file_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Pasirink_file_button.Name = "Pasirink_file_button";
+            this.Pasirink_file_button.Size = new System.Drawing.Size(112, 35);
+            this.Pasirink_file_button.TabIndex = 3;
+            this.Pasirink_file_button.Text = "Pasirink faila";
+            this.Pasirink_file_button.UseVisualStyleBackColor = true;
+            this.Pasirink_file_button.Click += new System.EventHandler(this.Pasirink_file_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 181);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(410, 244);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Copy_button
+            // 
+            this.Copy_button.Location = new System.Drawing.Point(166, 573);
+            this.Copy_button.Name = "Copy_button";
+            this.Copy_button.Size = new System.Drawing.Size(133, 54);
+            this.Copy_button.TabIndex = 8;
+            this.Copy_button.Text = "Kopijuoti!";
+            this.Copy_button.UseVisualStyleBackColor = true;
+            this.Copy_button.Click += new System.EventHandler(this.Copy_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 888);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Copy_button);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pasirink_file_button);
+            this.Controls.Add(this.Iki_sav_label);
+            this.Controls.Add(this.Nuo_sav_label);
+            this.Controls.Add(this.Iki_sav_textbox);
+            this.Controls.Add(this.Nuo_sav_textbox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Failu kopijuoklis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Nuo_sav_textbox;
+        private System.Windows.Forms.TextBox Iki_sav_textbox;
+        private System.Windows.Forms.Label Nuo_sav_label;
+        private System.Windows.Forms.Label Iki_sav_label;
+        private System.Windows.Forms.Button Pasirink_file_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Copy_button;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ExcelCopy/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ExcelCopy
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            label2.MaximumSize = new Size(400, 0);
+            label2.AutoSize = true;
+            label3.AutoSize = true;
+        }
+
+        private void Pasirink_file_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Multiselect = false; openFileDialog1.Filter = "All Files (*.*)|*.*";
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var File_name = System.IO.Path.GetFileName(openFileDialog1.FileName);
+                var DirectoryPath = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+                string NewFileName = "W"+Nuo_sav_textbox.Text + " " + File_name;
+                label1.Text = File_name;
+                label2.Text = DirectoryPath;
+           
+                //konvertuojam textboxu inputis i int
+                var x = int.Parse(Nuo_sav_textbox.Text);
+                var y = int.Parse(Iki_sav_textbox.Text);
+            
+                while (x < y+1)
+                {
+                    listBox1.Items.Add("W "+x+ " "+ File_name);
+                    x++;
+                }
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Nuo_sav_textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Leidzia tik skaicius
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Iki_sav_textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Leidzia tik skaicius
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void Copy_button_Click(object sender, EventArgs e)
+        {
+            var a = label2.Text + @"\" + label1.Text;
+            label3.Text = a;
+            foreach (var item in listBox1.Items)
+            {
+               //System.IO.File.Copy(label2.Text + @"\" + label1.Text, label2.Text + item.ToString());
+               label3.Text += item.ToString();
+            }
+        }
+    }
+}
