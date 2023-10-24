6a7ab1f4919f4a49d3a4df40eece45f3883404a1
Sysas <liudas_rolandas@yahoo.com>
2023-10-24 21:04:30 +0300

    Galutinis, turbut :)

--- ExcelCopy/MainForm.Designer.cs
+++ ExcelCopy/MainForm.Designer.cs
@@ -39,6 +39,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.Copy_button = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.Listbox1_clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Nuo_sav_textbox
@@ -48,6 +49,7 @@
             this.Nuo_sav_textbox.Name = "Nuo_sav_textbox";
             this.Nuo_sav_textbox.Size = new System.Drawing.Size(161, 26);
             this.Nuo_sav_textbox.TabIndex = 1;
+            this.Nuo_sav_textbox.TextChanged += new System.EventHandler(this.Nuo_sav_textbox_TextChanged);
             this.Nuo_sav_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Nuo_sav_textbox_KeyPress);
             // 
             // Iki_sav_textbox
@@ -57,6 +59,7 @@
             this.Iki_sav_textbox.Name = "Iki_sav_textbox";
             this.Iki_sav_textbox.Size = new System.Drawing.Size(161, 26);
             this.Iki_sav_textbox.TabIndex = 2;
+            this.Iki_sav_textbox.TextChanged += new System.EventHandler(this.Iki_sav_textbox_TextChanged);
             this.Iki_sav_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Iki_sav_textbox_KeyPress);
             // 
             // Nuo_sav_label
@@ -98,7 +101,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Location = new System.Drawing.Point(8, 754);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 4;
@@ -107,7 +110,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 470);
+            this.label2.Location = new System.Drawing.Point(8, 783);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 6;
@@ -119,14 +122,14 @@
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(16, 181);
+            this.listBox1.Location = new System.Drawing.Point(16, 133);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(410, 244);
+            this.listBox1.Size = new System.Drawing.Size(410, 596);
             this.listBox1.TabIndex = 7;
             // 
             // Copy_button
             // 
-            this.Copy_button.Location = new System.Drawing.Point(166, 573);
+            this.Copy_button.Location = new System.Drawing.Point(145, 805);
             this.Copy_button.Name = "Copy_button";
             this.Copy_button.Size = new System.Drawing.Size(133, 54);
             this.Copy_button.TabIndex = 8;
@@ -137,17 +140,28 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 509);
+            this.label3.Location = new System.Drawing.Point(12, 822);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 20);
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
+            // 
+            // Listbox1_clear
+            // 
+            this.Listbox1_clear.Location = new System.Drawing.Point(291, 735);
+            this.Listbox1_clear.Name = "Listbox1_clear";
+            this.Listbox1_clear.Size = new System.Drawing.Size(135, 29);
+            this.Listbox1_clear.TabIndex = 10;
+            this.Listbox1_clear.Text = "Išvalyti sąrašą";
+            this.Listbox1_clear.UseVisualStyleBackColor = true;
+            this.Listbox1_clear.Click += new System.EventHandler(this.Listbox1_clear_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(438, 888);
+            this.Controls.Add(this.Listbox1_clear);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Copy_button);
             this.Controls.Add(this.listBox1);
@@ -162,6 +176,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "Failu kopijuoklis";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,5 +195,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button Copy_button;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Listbox1_clear;
     }
 }
--- ExcelCopy/MainForm.cs
+++ ExcelCopy/MainForm.cs
@@ -16,43 +16,48 @@ namespace ExcelCopy
         public MainForm()
         {
             InitializeComponent();
-            label2.MaximumSize = new Size(400, 0);
-            label2.AutoSize = true;
-            label3.AutoSize = true;
         }
 
         private void Pasirink_file_button_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Multiselect = false; openFileDialog1.Filter = "All Files (*.*)|*.*";
-            
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            openFileDialog1.Multiselect = true; openFileDialog1.Filter = "All Files (*.*)|*.*";
+  
+            //konvertuojam textboxu inputis i int
+            var x = int.Parse(Nuo_sav_textbox.Text);
+            var y = int.Parse(Iki_sav_textbox.Text);
+            if (x > y)
             {
-                var File_name = System.IO.Path.GetFileName(openFileDialog1.FileName);
-                var DirectoryPath = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
-                string NewFileName = "W"+Nuo_sav_textbox.Text + " " + File_name;
-                label1.Text = File_name;
-                label2.Text = DirectoryPath;
-           
-                //konvertuojam textboxu inputis i int
-                var x = int.Parse(Nuo_sav_textbox.Text);
-                var y = int.Parse(Iki_sav_textbox.Text);
-            
-                while (x < y+1)
+                MessageBox.Show("Pražios savaitė didesnė už pabaigos", "Klaida!");
+            }
+            else
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    listBox1.Items.Add("W "+x+ " "+ File_name);
-                    x++;
+                    foreach (String file in openFileDialog1.FileNames)
+                    {
+                        var File_name = System.IO.Path.GetFileName(file);
+                        var DirectoryPath = System.IO.Path.GetDirectoryName(file);
+                        var DirectoryPathAndFileName = System.IO.Path.Combine(DirectoryPath, File_name);
+                        label1.Text = File_name;
+                        label2.Text = DirectoryPath;
+                        label3.Text = DirectoryPathAndFileName;
+
+                        while (x < y + 1)
+                        {
+                            listBox1.Items.Add(@"\W" + x + " " + File_name);
+                            x++;
+                        }
+                    }
                 }
             }
-
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
 
         }
-
-        private void Nuo_sav_textbox_KeyPress(object sender, KeyPressEventArgs e)
+         private void Nuo_sav_textbox_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Leidzia tik skaicius
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
@@ -60,7 +65,17 @@ namespace ExcelCopy
                 e.Handled = true;
             }
         }
-
+        private void Nuo_sav_textbox_TextChanged(object sender, EventArgs e)
+        {
+            if (Iki_sav_textbox.Text.Trim() != "" && Nuo_sav_textbox.Text.Trim() != "")
+            {
+                Pasirink_file_button.Enabled = true;
+            }
+            else
+            {
+                Pasirink_file_button.Enabled = false;
+            }
+        }
         private void Iki_sav_textbox_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Leidzia tik skaicius
@@ -70,16 +85,45 @@ namespace ExcelCopy
             }
 
         }
+        private void Iki_sav_textbox_TextChanged(object sender, EventArgs e)
+        {
+            if (Iki_sav_textbox.Text.Trim()!="" && Nuo_sav_textbox.Text.Trim()!="")
+            { 
+                Pasirink_file_button.Enabled = true;
+            }
+            else
+            {
+                Pasirink_file_button.Enabled = false;
+            }
+        }
 
         private void Copy_button_Click(object sender, EventArgs e)
         {
-            var a = label2.Text + @"\" + label1.Text;
-            label3.Text = a;
-            foreach (var item in listBox1.Items)
+            for (int i = 0; i < listBox1.Items.Count; i++)
             {
-               //System.IO.File.Copy(label2.Text + @"\" + label1.Text, label2.Text + item.ToString());
-               label3.Text += item.ToString();
+                object item = listBox1.Items[i];
+                System.IO.File.Copy(label3.Text, label2.Text + item.ToString(), true);
             }
+            listBox1.Items.Clear();
+            MessageBox.Show("Failai nukopijuoti: " + label2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Pasirink_file_button.Enabled = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void Listbox1_clear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
